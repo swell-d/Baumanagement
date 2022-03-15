2b32955a54713b7b3cdb840a1345fb8737769799
Dmitry Rude <3952800@gmail.com>
2022-03-15 14:44:52 +0100

    Company and CompanyRole models created

--- Baumanagement/admin.py
+++ Baumanagement/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Company, CompanyRole
+
+admin.site.register(Company)
+admin.site.register(CompanyRole)
--- Baumanagement/models.py
+++ Baumanagement/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class CompanyRole(models.Model):
+    name = models.CharField(max_length=512)
+
+    def __str__(self):
+        return self.name
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=512)
+    contact = models.CharField(max_length=512)
+    email = models.CharField(max_length=512)
+    tel = models.CharField(max_length=512)
+    address = models.CharField(max_length=512)
+    role = models.ForeignKey(CompanyRole, on_delete=models.CASCADE)
+    create = models.DateTimeField('create')
+    edit = models.DateTimeField('last edit')
+
+    def __str__(self):
+        return self.name
