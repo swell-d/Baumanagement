4000d83d65d803b6f07f575bd042039c3680e158
Dmitry Rude <3952800@gmail.com>
2022-03-21 17:29:49 +0100

    test form created

--- Baumanagement/forms_bills.py
+++ Baumanagement/forms_bills.py
@@ -1,5 +1,15 @@
 from django import forms
 
+from Baumanagement.models import Bill
 
-class BillForm(forms.Form):
-    pass
+
+class BillForm(forms.ModelForm):
+    class Meta:
+        model = Bill
+        fields = Bill.fields()
+        widgets = {
+            'text': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 3
+            }),
+        }
