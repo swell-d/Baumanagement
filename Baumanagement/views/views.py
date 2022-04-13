4cce32884aa83678bd4c427e8a9537e24c3e6cc7
Dmitry Rude <3952800@gmail.com>
2022-04-13 13:08:34 +0200

    performance improved

--- Baumanagement/views/views.py
+++ Baumanagement/views/views.py
@@ -71,36 +71,36 @@ def generate_objects_table(request, context, baseClass, tableClass, formClass, q
         context.setdefault('breadcrumbs', [{'text': _("All")}])
         new_object_form(request, context, formClass)
         context['search_field'] = True
-    else:
-        if queryset is None:
-            queryset = baseClass.objects
-        dateFrom = request.GET.get('dateFrom')
-        if dateFrom:
-            queryset = queryset.filter(created__gte=datetime.strptime(dateFrom, "%Y-%m-%d"))
-        dateTo = request.GET.get('dateTo')
-        if dateTo:
-            queryset = queryset.filter(created__lt=datetime.strptime(dateTo, "%Y-%m-%d") + timedelta(days=1))
-        tag = request.GET.get('tag')
-        if tag:
-            queryset = queryset.filter(tag=int(tag))
+    # else:
+    if queryset is None:
+        queryset = baseClass.objects
+    dateFrom = request.GET.get('dateFrom')
+    if dateFrom:
+        queryset = queryset.filter(created__gte=datetime.strptime(dateFrom, "%Y-%m-%d"))
+    dateTo = request.GET.get('dateTo')
+    if dateTo:
+        queryset = queryset.filter(created__lt=datetime.strptime(dateTo, "%Y-%m-%d") + timedelta(days=1))
+    tag = request.GET.get('tag')
+    if tag:
+        queryset = queryset.filter(tag=int(tag))
 
-        project_id_str = request.GET.get('project')
-        project_id = int(project_id_str) if project_id_str else None
-        settings = Settings.objects.get_or_create(user=request.user)[0]
-        settings_ap_id = settings.active_project.id if settings.active_project else None
-        if project_id != settings_ap_id:
-            settings.active_project = Project.objects.get(id=project_id) if project_id else None
-            settings.save()
-        if project_id and baseClass.__name__ == 'Contract':
-            queryset = queryset.filter(project_id=project_id)
-        elif project_id and baseClass.__name__ in ['Bill', 'Payment']:
-            queryset = queryset.filter(contract__project_id=project_id)
+    project_id_str = request.GET.get('project')
+    project_id = int(project_id_str) if project_id_str else None
+    settings = Settings.objects.get_or_create(user=request.user)[0]
+    settings_ap_id = settings.active_project.id if settings.active_project else None
+    if project_id != settings_ap_id:
+        settings.active_project = Project.objects.get(id=project_id) if project_id else None
+        settings.save()
+    if project_id and baseClass.__name__ == 'Contract':
+        queryset = queryset.filter(project_id=project_id)
+    elif project_id and baseClass.__name__ in ['Bill', 'Payment']:
+        queryset = queryset.filter(contract__project_id=project_id)
 
-        queryset = baseClass.extra_fields(queryset)
-        queryset = add_search_field(queryset, request)
-        table1 = tableClass(queryset, order_by="-created")
-        RequestConfig(request).configure(table1)
-        context['table1'] = table1
+    queryset = baseClass.extra_fields(queryset)
+    queryset = add_search_field(queryset, request)
+    table1 = tableClass(queryset, order_by="-created")
+    RequestConfig(request).configure(table1)
+    context['table1'] = table1
 
 
 def generate_object_table(request, context, baseClass, tableClass, formClass, queryset):
@@ -114,11 +114,11 @@ def generate_object_table(request, context, baseClass, tableClass, formClass, qu
             comment['files'] = [File.objects.get(id=id) for id in comment['object'].file_ids]
         context['tables'].append({'titel': _('Comments'), 'count': len(comment_ids),
                                   'comments': comments, 'form': CommentFormClass(), 'files_form': []})
-    else:
-        queryset = baseClass.extra_fields(queryset)
-        table1 = tableClass(queryset, orderable=False, object_table=True)
-        RequestConfig(request).configure(table1)
-        context['table1'] = table1
+    # else:
+    queryset = baseClass.extra_fields(queryset)
+    table1 = tableClass(queryset, orderable=False, object_table=True)
+    RequestConfig(request).configure(table1)
+    context['table1'] = table1
 
 
 def generate_next_objects_table(request, context, baseClass, tableClass, queryset, titel=None):
