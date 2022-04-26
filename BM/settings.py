9e4d7376983726cb4ea585869ef30ce8c71d1a1e
swell-d <3952800@gmail.com>
2022-04-26 19:49:41 +0200

    try to use heroku DB

--- BM/settings.py
+++ BM/settings.py
@@ -81,9 +81,16 @@ WSGI_APPLICATION = 'BM.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 if 'HEROKU_APP' in os.environ:
-    import django_heroku
-
-    django_heroku.settings(locals())
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['DB_NAME'],
+            'USER': os.environ['DB_USER'],
+            'PASSWORD': os.environ['DB_PASSWORD'],
+            'HOST': os.environ['DB_HOST'],
+            'PORT': os.environ['DB_PORT']
+        }
+    }
 else:
     DATABASES = {
         'default': {
