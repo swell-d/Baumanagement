640d7c1ffaffa8e5c7420269958b99537bb6bbf0
Dmitry Rude <3952800@gmail.com>
2022-04-28 12:17:05 +0200

    bugfix media files urls

--- BM/urls.py
+++ BM/urls.py
@@ -15,16 +15,17 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path, re_path
-from django.views.static import serve
+from django.urls import include, path
 
 urlpatterns = i18n_patterns(
     path('', include('Baumanagement.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
-    re_path(r'media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 )
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'Baumanagement.views.views.my404'
