c8bfcf5a98cabadf42e8bc925ca18ce04d85b2e9
Dmitry Rude <3952800@gmail.com>
2022-04-28 14:27:19 +0200

    LOGIN_URL for languages

--- BM/settings.py
+++ BM/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 
+from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,6 +118,7 @@ LOCALE_PATHS = [
     os.path.join(BASE_DIR, "locale"),
     os.path.join(BASE_DIR, "Baumanagement/locale"),
 ]
+LOGIN_URL = reverse_lazy('login')
 
 # Static files (CSS, JavaScript, Images)  https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_URL = 'static/'
