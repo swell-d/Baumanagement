978639bdfb6039079384e1b9b7939c5157bce715
Dmitry Rude <3952800@gmail.com>
2022-05-03 15:11:25 +0200

    refactoring

--- run_full.py
+++ run_full.py
@@ -5,7 +5,7 @@ from django.core.management import execute_from_command_line
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BM.settings')
-    execute_from_command_line(['manage.py', 'makemigrations', 'Baumanagement'])
+    execute_from_command_line(['manage.py', 'makemigrations'])
     execute_from_command_line(['manage.py', 'migrate'])
     execute_from_command_line(['manage.py', 'collectstatic', '--noinput'])
     execute_from_command_line(['manage.py', 'test'])
