8d66e8612bf6f98cea1efd82a0094d331dfeabc9
Dmitry Rude <3952800@gmail.com>
2022-05-03 08:45:44 +0200

    refactoring request.user|is_admin

--- Baumanagement/templatetags/auth_extras.py
+++ Baumanagement/templatetags/auth_extras.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import Group
 register = template.Library()
 
 
-@register.filter(name='has_group')
-def has_group(user, group_name):
-    group = Group.objects.get(name=group_name)
-    return True if group in user.groups.all() else False
+@register.filter(name='is_admin')
+def is_admin(user):
+    group = Group.objects.get_or_create(name='admins')[0]
+    return group in user.groups.all()
