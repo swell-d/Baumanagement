783b70ee6c9dd8f988e44d6a486b170fee5daa64
Dmitry Rude <3952800@gmail.com>
2022-05-06 15:12:29 +0200

    run_tests

---
+++ run_tests.py
@@ -0,0 +1,13 @@
+import os
+
+from django.core.management import execute_from_command_line
+
+
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'APP.settings')
+    execute_from_command_line(['manage.py', 'test'])
+    os.system('python manage.py runserver')
+
+
+if __name__ == '__main__':
+    main()
